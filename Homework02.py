9a653b91981b151c6a2b43a753f589736eee92d6
SonseungHyun1 <144421186+SonseungHyun1@users.noreply.github.com>
2023-12-09 20:50:15 +0900

    CP02.2차 과제 제출
    2차 과제 제출입니다!

--- Homework02.py
+++ Homework02.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 # 데이터 불러오기
 data_path = r"..\data\Catalunya Accidents data.csv"
 data = pd.read_csv(data_path)
+# 데이터 불러오기
+data_path = r"..\data\Catalunya Accidents data.csv"
+data = pd.read_csv(data_path)
 
+# 데이터 전처리 과정
 # 데이터 전처리 과정
 # D_BOIRA
 data['D_BOIRA'] = data['D_BOIRA'].map({"No n'hi ha": 0, "Si": 1})
@@ -32,6 +36,13 @@ selected_features = ["F_VICTIMES", "C_VELOCITAT_VIA", "D_BOIRA", "D_GRAVETAT", "
 data = data[selected_features]
 data = data.fillna(data.mean())
 
+# 필요한 특징 선택 (피해자 수만 남김)
+selected_features = ["F_VICTIMES", "C_VELOCITAT_VIA", "D_BOIRA", "D_GRAVETAT", "D_CLIMATOLOGIA", "D_INFLUIT_VISIBILITAT"]
+data = data[selected_features]
+
+# 데이터 전처리 - 필요한 특징 선택 및 누락된 값 처리
+data = data.fillna(data.mean())
+
 # 특징과 레이블 분리
 X = data.drop("F_VICTIMES", axis=1)
 y = data["F_VICTIMES"]
@@ -39,16 +50,36 @@ y = data["F_VICTIMES"]
 # 학습용과 테스트용으로 데이터 분할
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 의사결정 나무 모델 구축 및 학습
-dt_model = DecisionTreeClassifier(random_state=42)
-dt_model.fit(X_train, y_train)
+# 랜덤 포레스트 모델 구축 및 학습
+rf_model = RandomForestClassifier(random_state=42)
+rf_model.fit(X_train, y_train)
 
-# 테스트 데이터로 의사결정 나무 모델 평가
-y_pred_dt = dt_model.predict(X_test)
-accuracy_dt = accuracy_score(y_test, y_pred_dt)
-print(f"의사결정 나무 모델 정확도: {accuracy_dt}")
+# 테스트 데이터로 랜덤 포레스트 모델 평가
+y_pred_rf = rf_model.predict(X_test)
+accuracy_rf = accuracy_score(y_test, y_pred_rf)
+print(f"랜덤 포레스트 모델 정확도: {accuracy_rf}")
 
 # 분류 보고서 출력
-classification_rep_dt = classification_report(y_test, y_pred_dt, zero_division=1)
-print("분류 보고서:\n", classification_rep_dt)
+classification_rep_rf = classification_report(y_test, y_pred_rf, zero_division=1)
+print("분류 보고서:\n", classification_rep_rf)
+
+import matplotlib.pyplot as plt
+
+# 특성 중요도 가져오기
+feature_importances = rf_model.feature_importances_
+
+# 특성 중요도를 내림차순으로 정렬
+indices = sorted(range(len(feature_importances)), key=lambda k: feature_importances[k], reverse=True)
+
+# 특성 이름 가져오기
+feature_names = X.columns
+
+# 시각화
+plt.figure(figsize=(12, 8))
+plt.bar(range(len(feature_importances)), feature_importances[indices], align="center")
+plt.xticks(range(len(feature_importances)), [feature_names[i] for i in indices], rotation=45)
+plt.xlabel("Feature")
+plt.ylabel("Importance")
+plt.title("Random Forest Feature Importance")
+plt.show()
 
